bcc49d0e4b6c76a4a737e84167d8e0011097f0c3
Jesse Billard <jessebillard@mac.com>
2018-03-13 17:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function viewCart() {
       
   if (cart.length >= 3) {
     var lastItem = list.splice(-1);
-    list.splice(-1)
+    list.pop()
     console.log(intro + list.join(', ') + 'and ' + lastItem + '.' ) 
     }
    
